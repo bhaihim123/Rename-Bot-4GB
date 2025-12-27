5f7b74dd5e8a0cc771ae1043ba74ba54175924ae
bhaihim123 <bhaihim863@gmail.com>
2025-12-27 13:41:43 +0530

    Update metadata.py

--- plugins/metadata.py
+++ plugins/metadata.py
@@ -3,29 +3,44 @@ from pyrogram.types import Message, CallbackQuery, InlineKeyboardButton, InlineK
 from helper.database import *
 from pyromod.exceptions import ListenerTimeout
 from script import *
-
-
+from config import *
 
 ON = [[InlineKeyboardButton('Metadata On ✅', callback_data='metadata_1')], [
     InlineKeyboardButton('Set Custom Metadata', callback_data='cutom_metadata')]]
 OFF = [[InlineKeyboardButton('Metadata Off ❌', callback_data='metadata_0')], [
     InlineKeyboardButton('Set Custom Metadata', callback_data='cutom_metadata')]]
 
+# ===== USERBOT CLIENT =====
+app = Client(
+    "RenamerUser",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=STRING_SESSION
+)
 
-@Client.on_message(filters.private & filters.command('metadata'))
-async def handle_metadata(bot: Client, message: Message):
+@app.on_message(filters.private & filters.command('metadata'))
+async def handle_metadata(_, message: Message):
 
     ms = await message.reply_text("**Please Wait...**", reply_to_message_id=message.id)
     bool_metadata = find(int(message.chat.id))[2]
     user_metadata = find(int(message.chat.id))[3]
     await ms.delete()
+
     if bool_metadata:
-        return await message.reply_text(f"**Your Current Metadata :-**\n\n➜ `{user_metadata}` ",quote=True, reply_markup=InlineKeyboardMarkup(ON))
-    return await message.reply_text(f"**Your Current Metadata :-**\n\n➜ `{user_metadata}` ",quote=True, reply_markup=InlineKeyboardMarkup(OFF))
+        return await message.reply_text(
+            f"**Your Current Metadata :-**\n\n➜ `{user_metadata}` ",
+            quote=True,
+            reply_markup=InlineKeyboardMarkup(ON)
+        )
 
+    return await message.reply_text(
+        f"**Your Current Metadata :-**\n\n➜ `{user_metadata}` ",
+        quote=True,
+        reply_markup=InlineKeyboardMarkup(OFF)
+    )
 
-@Client.on_callback_query(filters.regex('.*?(custom_metadata|metadata).*?'))
-async def query_metadata(bot: Client, query: CallbackQuery):
+@app.on_callback_query(filters.regex('.*?(custom_metadata|metadata).*?'))
+async def query_metadata(_, query: CallbackQuery):
 
     data = query.data
 
@@ -35,31 +50,37 @@ async def query_metadata(bot: Client, query: CallbackQuery):
 
         if bool(eval(_bool)):
             setmeta(int(query.message.chat.id), bool_meta=False)
-            await query.message.edit(f"**Your Current Metadata :-**\n\n➜ `{user_metadata}` ", reply_markup=InlineKeyboardMarkup(OFF))
-
+            await query.message.edit(
+                f"**Your Current Metadata :-**\n\n➜ `{user_metadata}` ",
+                reply_markup=InlineKeyboardMarkup(OFF)
+            )
         else:
             setmeta(int(query.message.chat.id), bool_meta=True)
-            await query.message.edit(f"**Your Current Metadata :-**\n\n➜ `{user_metadata}` ", reply_markup=InlineKeyboardMarkup(ON))
+            await query.message.edit(
+                f"**Your Current Metadata :-**\n\n➜ `{user_metadata}` ",
+                reply_markup=InlineKeyboardMarkup(ON)
+            )
 
     elif data == 'cutom_metadata':
         await query.message.delete()
         try:
             try:
-                metadata = await bot.ask(text=script.METADATA_TXT, chat_id=query.from_user.id, filters=filters.text, timeout=30, disable_web_page_preview=True, reply_to_message_id=query.message.id)
+                metadata = await app.ask(
+                    chat_id=query.from_user.id,
+                    text=script.METADATA_TXT,
+                    filters=filters.text,
+                    timeout=30,
+                    disable_web_page_preview=True
+                )
             except ListenerTimeout:
-                await query.message.reply_text("⚠️ Error !!\n\n**Request Timed Out.**\n\nRestart By Using /metadata", reply_to_message_id=query.message.id)
+                await query.message.reply_text(
+                    "⚠️ Error !!\n\n**Request Timed Out.**\n\nRestart By Using /metadata"
+                )
                 return
-            print(metadata.text)
-            ms = await query.message.reply_text("**Please Wait...**", reply_to_message_id=metadata.id)
+
+            ms = await query.message.reply_text("**Please Wait...**")
             setmetacode(int(query.message.chat.id), metadata_code=metadata.text)
             await ms.edit("**Your Metadata Code Set Successfully ✅**")
+
         except Exception as e:
             print(e)
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @JishuBotz
-# Developer @JishuDeveloper
