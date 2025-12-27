0a3ead5e42be2a3b5cc4120edf640eed6dd25074
bhaihim123 <bhaihim863@gmail.com>
2025-12-27 12:49:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 from pyrogram import Client, idle
-from plugins.cb_data import app as Client2
 from config import *
 import pyromod
 import pyrogram.utils
@@ -7,29 +6,14 @@ import pyrogram.utils
 pyrogram.utils.MIN_CHAT_ID = -999999999999
 pyrogram.utils.MIN_CHANNEL_ID = -100999999999999
 
+app = Client(
+    "RenamerUser",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=STRING_SESSION,
+    plugins=dict(root="plugins")
+)
 
-
-bot = Client("Renamer", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH, plugins=dict(root='plugins'))
-
-
-
-
-if STRING_SESSION:
-    apps = [Client2,bot]
-    for app in apps:
-        app.start()
-    idle()
-    for app in apps:
-        app.stop()
-    
-else:
-    bot.run()
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Back-Up Channel @JishuBotz
-# Developer @JishuDeveloper
+app.start()
+idle()
+app.stop()
