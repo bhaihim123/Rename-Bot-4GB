d3ee30e8b0c54e7fc36b317307efb32c12517506
bhaihim123 <bhaihim863@gmail.com>
2025-12-27 14:31:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
-from plugins.cb_data import app
-from pyrogram import idle
+import pyromod.listen  # 🔥 MUST (না দিলে listener error আসবে)
 import pyrogram.utils
+from pyrogram import idle
 
-# Telegram ID limit fix (optional but safe)
+from plugins.cb_data import app
+
+# Telegram ID limit fix (safe)
 pyrogram.utils.MIN_CHAT_ID = -999999999999
 pyrogram.utils.MIN_CHANNEL_ID = -100999999999999
 
