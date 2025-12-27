409f4031948b55c08590f038154ff4380e47682b
bhaihim123 <bhaihim863@gmail.com>
2025-12-27 14:15:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,12 @@
-from pyrogram import Client, idle
-from config import *
-import pyromod
+from plugins.cb_data import app
+from pyrogram import idle
 import pyrogram.utils
 
+# Telegram ID limit fix (optional but safe)
 pyrogram.utils.MIN_CHAT_ID = -999999999999
 pyrogram.utils.MIN_CHANNEL_ID = -100999999999999
 
-app = Client(
-    "RenamerUser",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    session_string=STRING_SESSION,
-    plugins=dict(root="plugins")
-)
+print("✅ Renamer Userbot Started")
 
 app.start()
 idle()
